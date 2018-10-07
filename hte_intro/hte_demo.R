bac745b4fb41f571501a16563428fb23b42b9c9f
markhwhiteii <markhwhiteii@gmail.com>
2018-10-07 13:02:10 -0400

    sun morning

--- hte_intro/hte_demo.R
+++ hte_intro/hte_demo.R
@@ -17,28 +17,42 @@ dat <- read_tsv("broockman_kalla_replication_data.tab") %>%
   ) %>% 
   filter(complete.cases(.))
 
+# make lgl for middle
+tiles <- quantile(dat$trans_therm_pre)
+lgl <- dat$trans_therm_pre >= tiles[[2]] & dat$trans_therm_pre <= tiles[[4]]
+dat$middle <- lgl * 1
+
 set.seed(1839)
-cases <- sample(seq_len(nrow(dat)), round(nrow(dat) / 2))
+cases <- sample(seq_len(nrow(dat)), round(nrow(dat) * .6))
 train <- dat[cases, ]
 test <- dat[-cases, ]
 
 cf <- causal_forest(
-  X = model.matrix(~ ., data = train[, 3:ncol(train)]),
+  X = model.matrix(~ ., data = train[, 4:ncol(train)]),
   Y = train$trans_therm_post,
   W = as.numeric(train$treatment) - 1,
-  num.trees = 10000,
+  num.trees = 5000,
   seed = 1839
 )
 
-preds <- predict(cf, model.matrix(~ ., data = test[, 3:ncol(test)]))
-test$preds <- c(preds$predictions)
-
+# generate predictions
+preds <- predict(
+  object = cf, 
+  newdata = model.matrix(~ ., data = test[, 3:ncol(test)]), 
+  estimate.variance = TRUE
+)
+test$preds <- preds$predictions[, 1]
+  
 # get top 1/2 of preds
 t1 <- lm(trans_therm_post ~ treatment, test[test$preds > median(test$preds), ])
 summary(t1)
 
 # get middle 1/2 of original
-tiles <- quantile(test$trans_therm_pre)
-lgl <- test$trans_therm_pre >= tiles[[2]] & test$trans_therm_pre <= tiles[[4]]
-t2 <- lm(trans_therm_post ~ treatment, test[lgl, ])
+t2 <- lm(trans_therm_post ~ treatment, test[test$middle == 1, ])
 summary(t2)
+
+# get random
+set.seed(1839)
+cases <- sample(seq_len(nrow(test)), round(nrow(test) * .5))
+t3 <- lm(trans_therm_post ~ treatment, test[cases, ])
+summary(t3)
