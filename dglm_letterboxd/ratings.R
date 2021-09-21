707fa7744ad75bebea23587e3026f0135171603f
Mark H. White II <markhwhiteii@gmail.com>
2021-09-21 15:07:51 -0500

    update files for posting

--- dglm_letterboxd/ratings.R
+++ dglm_letterboxd/ratings.R
@@ -1,31 +1,50 @@
-library(dglm)
+library(gamlss)
 library(tidyverse)
 
-dat <- read_csv("ratings.csv")
+# show with fake data ----------------------------------------------------------
+n <- 100000
+set.seed(1839)
+x <- runif(n)
+z <- runif(n)
 
-m1 <- lm(Rating ~ Year, dat)
+mu <- 0.5 + 2 * x
+sigma <- exp(1.5 + 3 * z)
+
+y <- rnorm(n, mu, sigma)
+dat <- data.frame(x, z, y)
+
+m0 <- gamlss(y ~ x + z, ~ x + z, family = NO(), data = dat)
+round(coef(m0, "mu"), 1)
+round(coef(m0, "sigma"), 1)
+
+# use letterboxd data ----------------------------------------------------------
+ratings <- read_csv("ratings.csv") %>% 
+  janitor::clean_names()
+
+ggplot(ratings, aes(x = year, y = rating)) +
+  geom_count() +
+  geom_smooth(method = "lm", formula = y ~ x, se = FALSE, size = 1.5, color = ) +
+  theme_light() +
+  labs(x = "Year", y = "Rating") +
+  scale_x_continuous(breaks = seq(1930, 2020, by = 10)) +
+  theme(text = element_text(size = 16))
+
+m1 <- gamlss(rating ~ year, ~ year, family = NO(), data = ratings)  
 summary(m1)
 
-ggplot(dat, aes(x = Year, y = Rating)) +
-  geom_count() +
-  geom_smooth(method = "lm", formula = y ~ x) +
-  theme_light()
+plot_data <- data.frame(year = min(ratings$year):max(ratings$year))
+pred_mu <- predict(m1, "mu", newdata = plot_data)
+pred_sigma <- predict(m1, "sigma", newdata = plot_data, type = "response")
+plot_data$mu <- pred_mu
+plot_data$sigma <- pred_sigma
 
-m2 <- dglm(Rating ~ Year, ~ Year, data = dat, method = "reml")
-summary(m2, dispersion = 2)
-
-ggplot(mapping = aes(x = dat$Year, y = m2$dispersion.fit$fitted.values)) +
-  geom_line() +
+ggplot(plot_data, aes(x = year, y = mu)) +
+  geom_count(data = ratings, mapping = aes(x = year, y = rating)) +
+  geom_line(size = 1.25) +
+  geom_line(aes(y = mu + sigma), linetype = 2) +
+  geom_line(aes(y = mu - sigma), linetype = 2) +
   theme_light() +
-  labs(x = "Year", y = "Predicted Variance")
-
-m3 <- dglm(Rating ~ Year, ~ splines::bs(Year), data = dat, method = "reml")
-summary(m3, dispersion = 2)
-
-m2$m2loglik
-m3$m2loglik
-
-ggplot(mapping = aes(x = dat$Year, y = m3$dispersion.fit$fitted.values)) +
-  geom_line() +
-  theme_light() +
-  labs(x = "Year", y = "Predicted Variance")
+  labs(x = "Year", y = "Rating") +
+  scale_x_continuous(breaks = seq(1930, 2020, by = 10)) +
+  theme(text = element_text(size = 16)) +
+  ylim(c(0.5, 5))
